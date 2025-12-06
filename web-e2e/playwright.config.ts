89fa53f03d91cf22f95e6dc09eff32e89dd7760f
MohamedEMonem <sim.mohamedessam44975@alexu.edu.eg>
2025-12-06 02:42:37 +0200

    chore: update configurations to use .cjs files and correct project references

--- web-e2e/playwright.config.ts
+++ web-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run web:serve',
+    command: 'npx nx run @omni-store/web:serve:development',
     url: 'http://localhost:4200',
     reuseExistingServer: true,
     cwd: workspaceRoot,
