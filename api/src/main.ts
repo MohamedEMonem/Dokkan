2f360a871d7bfc7759eee54b2f33ca08d9c10e33
MohamedEMonem <sim.mohamedessam44975@alexu.edu.eg>
2026-02-04 02:42:10 +0200

    feat : remove nestjs api and add expressjs api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,46 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ValidationPipe, Logger } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-// 1. Import Swagger classes
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap');
+import express from 'express';
+import * as path from 'path';
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+const app = express();
 
-  app.useGlobalFilters(new HttpExceptionFilter());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to api!' });
+});
 
-  // 2. Setup Swagger Configuration
-  const config = new DocumentBuilder()
-    .setTitle('Graduation Project API') // Title requested
-    .setDescription('The Omni-Store API description')
-    .setVersion('1.0')
-    .addTag('omni-store')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  // 3. Setup the Swagger UI route
-  SwaggerModule.setup('api/docs', app, document);
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  logger.log(`Application is running on: http://localhost:${port}`);
-  logger.log(`Swagger Docs available at: http://localhost:${port}/api/docs`);
-}
-bootstrap();
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
