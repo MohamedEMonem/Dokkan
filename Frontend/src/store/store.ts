d57dc0bf944f808697bfee8c0b79068bfab00f26
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-02-28 01:33:12 +0200

    Refactor store setup: move typed hooks to new hooks.ts file and clean up store.ts

--- Frontend/src/store/store.ts
+++ Frontend/src/store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './apiSlice';
-import { useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
@@ -10,10 +9,5 @@ export const store = configureStore({
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
