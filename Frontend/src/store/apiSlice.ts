e9cb302568eb70fa2a32eedc228b185aa1cba018
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-02-28 01:09:32 +0200

    Rename reducerPath from 'api' to 'apiSlice' for clarity

--- Frontend/src/store/apiSlice.ts
+++ Frontend/src/store/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
 const token : string | null = localStorage.getItem('token'); // TBF later;
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
+  reducerPath: 'apiSlice',
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.SERVER_DEV_API_URL,
     headers: {
