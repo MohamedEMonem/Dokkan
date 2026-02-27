21dfcdcad9ae751107ff61f1b75582d03659551d
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-02-28 01:25:41 +0200

    Fix import statement for createApi in apiSlice to include 'react' for correct functionality

--- Frontend/src/store/apiSlice.ts
+++ Frontend/src/store/apiSlice.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const token : string | null = localStorage.getItem('token'); // TBF later;
 
