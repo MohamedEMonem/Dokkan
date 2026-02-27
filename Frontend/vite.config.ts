27bde4de00347df078af00fb9ed29218b40dd2b3
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-02-28 00:57:42 +0200

    Configure Vite to load environment variables and set server port dynamically

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env : any = loadEnv(mode, process.cwd(), '');
+  const PORT : number = parseInt(env.PORT);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      port: PORT,
+    },
+    preview: {
+      port: PORT,
+    },
+  }
 })
