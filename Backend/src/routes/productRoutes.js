0125eb73755ee2fa0cbd01a53e901ddd39e653c0
MIRIKO2022 <eraky2249@gmail.com>
2026-04-08 04:03:50 +0200

    fix product update and delete routes

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -14,7 +14,6 @@ router.get("/", ProductController.getProducts);
 
 // router.post("/", upload.single("product"),productImgUploadHandler,ProductController.createProduct);
 router.post("/",auth,authStoreOwner,upload.single("image"),ProductController.createProduct);
-router.patch("/:id", ProductController.updateProduct);
-router.delete("/:id", ProductController.deleteProduct);
-
+router.patch("/:id", auth, authStoreOwner, upload.single("image"), ProductController.updateProduct);
+router.delete("/:id", auth, authStoreOwner, ProductController.deleteProduct);
 module.exports = router;
