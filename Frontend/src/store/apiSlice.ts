ea06047112b57866e29fea59dd506c596d465da2
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-05-11 01:35:27 +0300

    fix: update environment variable references for API URL in Dockerfile and apiSlice

--- Frontend/src/store/apiSlice.ts
+++ Frontend/src/store/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'apiSlice',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_SERVER_DEV_API_URL,
+    baseUrl: import.meta.env.VITE_SERVER_API_URL || import.meta.env.VITE_SERVER_DEV_API_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
       if (token) {
