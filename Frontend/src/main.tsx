eb2f5d1f407217437ed34555eb8ef6425644fb2c
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-06-23 17:40:55 +0300

    revert: Google authentication frontend changes from merge commit 3d5c26b

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from '@/store/store.ts'
 import { Notification } from '@/components/ui/Notification.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <App />
-        <Notification />
-      </GoogleOAuthProvider>
+      <App />
+      <Notification />
     </Provider>
   </StrictMode>,
 )
